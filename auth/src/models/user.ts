b72b038776025546f93381e42a3b076ca849ead5
ckeller22 <chriskeller222@gmail.com>
2021-08-14 15:18:58 -0700

    begin to implement mongoose

---
+++ auth/src/models/user.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+// Implement TypeScript type checking for users
+interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+	email: string;
+	password: string;
+}
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+// Type checking for mongoose schema
+userSchema.statics.build = (attrs: UserAttrs) => {
+	return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
