c4fa87e78aa0a8e4ef71899801b4f63cc9444004
ckeller22 <chriskeller222@gmail.com>
2021-08-14 16:36:59 -0700

    implement password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // Implement TypeScript type checking for users
 interface UserAttrs {
@@ -24,6 +25,14 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+});
+
+userSchema.pre("save", async function (done) {
+	if (this.isModified("password")) {
+		const hashed = await Password.toHash(this.get("password"));
+		this.set("password", hashed);
+	}
+	done();
 });
 
 // Type checking for mongoose schema
