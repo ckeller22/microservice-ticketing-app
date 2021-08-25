b34253e3ac760284b8b40105526ce4b8b764d845
ckeller22 <chriskeller222@gmail.com>
2021-08-25 15:27:52 -0700

    begin work on nats streaming server

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,29 @@
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
+console.clear();
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
+
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({ id: "123", title: "weeee", price: 20000000 });
+  } catch (err) {
+    console.error(err);
+  }
+
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "Hello",
+  //   price: 20,
+  // });
+
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event published");
+  // });
+});
