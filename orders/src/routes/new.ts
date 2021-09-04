13cf0d0233ea58cefa121beb1750e9dde5aa72a6
ckeller22 <chriskeller222@gmail.com>
2021-09-04 16:40:29 -0700

    removed check in orders/new.ts

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -21,13 +21,7 @@ const EXPIRATION_WINDOW_SECONDS = 1 * 60;
 router.post(
   "/api/orders",
   requireAuth,
-  [
-    body("ticketId")
-      .not()
-      .isEmpty()
-      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-      .withMessage("TicketId must be provided"),
-  ],
+  [body("ticketId").not().isEmpty().withMessage("TicketId must be provided")],
   validateRequest,
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
