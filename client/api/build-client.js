d3b20beb5a4976869164f763bc812a777e24806e
ckeller22 <chriskeller222@gmail.com>
2021-09-08 09:34:29 -0700

    fixed building project locally in skaffold

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 
 const buildClient = ({ req }) => {
-  if (typeof window === "undefined") {
-    // On server
+	if (typeof window === "undefined") {
+		// On server
+		const url = getURL();
+		console.log(getURL());
+		return axios.create({
+			baseURL: url,
+			headers: req.headers,
+		});
+	} else {
+		// In browser
 
-    return axios.create({
-      baseURL: "http://www.tixgit.website",
-      headers: req.headers,
-    });
-  } else {
-    // In browser
+		return axios.create({});
+	}
+};
 
-    return axios.create({});
-  }
+const getURL = () => {
+	// Checks env var defined in skaffold to set host dynamically for local development
+	let url;
+	if (process.env.DEV_MODE === "true") {
+		url = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+	} else {
+		url = "http://www.tixgit.website";
+	}
+	return url;
 };
 
 export default buildClient;
